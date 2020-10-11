5551777e238d50dc28bc4b97cee1d9f07a9da34f
ryota-sb <ryota.sb.1206@gmail.com>
2020-10-11 23:08:44 +0900

    vuex tasks module 修正

--- src/store/modules/task.js
+++ src/store/modules/task.js
@@ -1,5 +1,6 @@
 import axios  from 'axios'
 import router from '../../router/index'
+import RestAPI from '../../plugins/rest_api'
 
 export const tasks = {
   namespaced: true,
@@ -11,48 +12,61 @@ export const tasks = {
   },
   mutations: {
     getTasks(state, value) {
-      state.tasks = value;
+      state.tasks = value
     },
     createTask(state, value) {
-      state.tasks.push(value);
+      // state.tasks.push(value)
+      console.log(value)
     },
     updateTask(state, value) {
-      state.tasks = value;
+      state.tasks = value
     },
     deleteTask(state, index) {
-      state.tasks.splice(index, 1);
+      state.tasks.splice(index, 1)
     }
   },
   actions: {
-    getTasksAction({ commit }) {
-      axios.get('/api/v1/tasks').then(response => {
-        commit('getTasks', response.data);
-      }).catch(error => {
-        console.log(error);
+    async getTasksAction({ commit }) {
+      const instance = axios.create({
+        baseURL: RestAPI.url()
+      })
+      await instance.get('/api/v1/tasks').then((response) => {
+        commit('getTasks', response.data)
+      }).catch((error) => {
+        console.log(error)
       })
     },
-    createTaskAction({ commit }, { newTask, week, time, user_id }) {
-      if (newTask == '' || newTask.length > 40) { return }
-      axios.post('/api/v1/tasks', { task: { content: newTask, week: week, notification_time: time, user_id: user_id } }).then(response => {
-        commit('createTask', response.data);
-      }).catch(error => {
-        console.log(error);
+    async createTaskAction({ commit }, { newTask, week, time, user_id }) {
+      if (newTask == null || newTask.length > 40) return
+      const instance = axios.create({
+        baseURL: RestAPI.url()
+      })
+      await instance.post('/api/v1/tasks', { task: { content: newTask, week: week, notification_time: time, user_id: user_id } }).then((response) => {
+        commit('createTask', response.data)
+      }).catch((error) => {
+        console.log(error)
       })
     },
-    updateTaskAction({ commit }, { task_id }) {
-      axios.put('/api/v1/tasks/' + task_id).then(response => {
-        commit('updateTask', response.data);
+    async updateTaskAction({ commit }, { task_id }) {
+      const instance = axios.create({
+        baseURL: RestAPI.url()
+      })
+      await instance.put('/api/v1/tasks/' + task_id).then((response) => {
+        commit('updateTask', response.data)
         router.go({ path: router.currentRoute.path, force: true })
-      }).catch(error => {
-        console.log(error);
+      }).catch((error) => {
+        console.log(error)
       })
     },
-    deleteTaskAction({ commit }, { task_id, index }) {
-      axios.delete('/api/v1/tasks/' + task_id).then(response => {
-        commit('deleteTask', index);
+    async deleteTaskAction({ commit }, { task_id, index }) {
+      const instance = axios.create({
+        baseURL: RestAPI.url()
+      })
+      await instance.delete('/api/v1/tasks/' + task_id).then((response) => {
+        commit('deleteTask', index)
         console.log(response)
-      }).catch(error => {
-        console.log(error);
+      }).catch((error) => {
+        console.log(error)
       })
     }
   }
