f42524bd14f0df8d2085b195af9a43c19ad11bb6
ryota-sb <ryota.sb.1206@gmail.com>
2020-10-24 17:00:57 +0900

    flash message layout

---
+++ src/components/FlashMessage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row
+    justify="center"
+  >
+    <v-col
+      v-if="message"
+      xs="10"
+      sm="10"
+      md="10"
+      lg="10"
+    >
+      <v-alert
+        dense
+        text
+        type="success"
+        class="ma-0"
+        v-text="message"
+      />
+    </v-col>
+    <v-spacer v-else />
+  </v-row>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      message: state => state.flashMessage.message
+    })
+  }
+}
+</script>
